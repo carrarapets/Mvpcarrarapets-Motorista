6fca1cafe5047525635916997c61097f4c07b61c
Guuisr07 <guisantana0204@gmail.com>
2022-09-22 00:09:52 -0300

    Checkpoint

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,4 @@
-import express, {Request, Response, NextFunction} from 'express';
+import express from "express"; 
 import cors from 'cors';
 import { router } from './routes/routes';
 
@@ -6,20 +6,21 @@ const app = express();
 app.use(cors())
 app.use(express.json())
 app.use(router);
-app.get("/", (req, res) => {
-    return res.json("hello word");
+
+app.get("/", (request, response) => {
+    return response.json("hello word");
 
 });
-app.use((err: Error, req: Request, res: Response, next: NextFunction)=>{
-    if(err instanceof Error){
-        return res.status(400).json({
-            error: err.message
-        })
-    }
-    return res.status(500).json({
-        status: 'error',
-        message: 'Erro no servidor'
-    })
+app.use((err, request, response)=>{
+    // if(err instanceof Error){
+    //     return response.status(400).json({
+    //         error: err.message
+    //     })
+    // }
+    // return response.status(500).json({
+    //     status: 'error',
+    //     message: 'Erro no servidor'
+    // })
 })
 
 app.listen(3334, ()=> console.log("servidor online"))
